0c8391e11250a645e3a20d94e956f3d3e6eab998
Jess <jess2410@hotmail.fr>
2022-11-21 22:42:04 +0100

    v1 - final commit

--- components/Projects/ProjectModal.jsx
+++ components/Projects/ProjectModal.jsx
@@ -1,27 +1,45 @@
 import styles from "./Modal.module.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { BASE_URI } from "../../public/assets/app.config";
 
 export const ProjectModal = ({ project, onClose }) => {
   console.log("projectModal", project);
+
+  const host_url = `${process.env.VERCEL_URL}/`;
+
+  const router = useRouter();
   return (
     <div className={styles.modalBg}>
       <div className={styles.modal}>
-        <div className={styles.modalContent}>
-          <div className={styles.services_iconBx}>
-            <h2 className={styles.services_h2}>{project?.name}</h2>
-            <Image
-              src={project.img}
-              layout='responsive'
-              width={570}
-              height={200}
-            />
-          </div>
-          <p className={styles.services_descrption}>{project?.description}</p>
+        <button className={styles.close} onClick={onClose}>
+          <Image src='/assets/close.png' width={20} height={20} alt='close' />
+        </button>
+        <div className={styles.iconBx}>
+          <Image
+            src={project.img}
+            layout='responsive'
+            width={570}
+            height={200}
+            alt={project.name}
+          />
         </div>
-        <div>
-          <button className={styles.modalBtn} onClick={onClose}>
-            Fermer
-          </button>
+        <div className={styles.textContent}>
+          <h2 className={styles.h2}>{project?.name}</h2>
+          <h3 className={styles.h3}>{project?.subtitle}</h3>
+          <p>{project?.description}</p>
+          <div className={styles.btnContent}>
+            <Link
+              className={styles.modalBtn}
+              rel='noopener'
+              href={project?.href}
+              //   passHref={true}
+              //   onClick={() => router.push(`${project?.href}`)}
+            >
+              View Website
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- data.js
+++ data.js
@@ -17,19 +17,75 @@ export const servicesData = {
 export const projectsData = {
   projects: [
     {
-      href: "www.stepadvise.fr",
+      href: "http://www.stepadvise.fr",
       name: "StepAdvise",
       img: "/assets/stepadvise.jpg",
-      description: "Il s&rsquo;agit d&rsquo;une description",
+      description:
+        "The company StepAdvise, specialized in urban communication, needed a simple and informative website. From the layout to the deployment, I took real pleasure in designing a site that corresponded to their image.",
     },
-    { href: "#", name: "RestoFood", img: "/assets/resto.jpg" },
-    { href: "#", name: "React Chat", img: "/assets/reactchat.jpeg" },
-    { href: "#", name: "Art Of Jess", img: "/assets/artjess.jpg" },
-    { href: "#", name: "Pet Store", img: "/assets/petshop.jpeg" },
-    { href: "#", name: "Drive In Green", img: "/assets/drive.jpg" },
-    { href: "#", name: "Brand", img: "/assets/brand.jpg" },
-    { href: "#", name: "Chapito", img: "/assets/chapito.jpeg" },
-    { href: "#", name: "Amazona", img: "/assets/amazona.jpg" },
+    {
+      href: "http://jess2410.github.io/Resto-Food/",
+      name: "RestoFood",
+      img: "/assets/resto.jpg",
+      description:
+        "This website was created as part of my training in order to improve myself in the field of front-end.",
+    },
+    {
+      href: "http://github.com/Jess2410/website-responsive",
+      name: "Brands",
+      img: "/assets/brands.png",
+      description:
+        "This website was created as part of my training in order to improve myself in the field of front-end.",
+    },
+    {
+      href: "http://github.com/Jess2410/chat-react-firebase",
+      name: "React Chat",
+      img: "/assets/reactchat.jpeg",
+      description:
+        "This website was created as part of my training in order to improve myself in the field of front-end.",
+    },
+    {
+      href: "http://github.com/Jess2410/artjess-website",
+      name: "Art Of Jess",
+      img: "/assets/artjess.jpg",
+      description:
+        "This project is my future art site. The one that embodies the marriage of my two passions: web development and digital design. E-commerce and Art Gallery, it will be deployed soon.",
+    },
+    {
+      href: "http://github.com/Jess2410/PetStore",
+      name: "Pet Store",
+      img: "/assets/petshop.jpeg",
+      description:
+        "This website was created as part of my training in order to improve myself in the field of front-end.",
+    },
+    {
+      href: "http://github.com/Jess2410/driveingreenfinal-prod",
+      name: "Drive In Green",
+      img: "/assets/drive.jpg",
+      description:
+        "This is a site made as part of an apprenticeship. Drive In Green is an electric car rental company. We worked in a team of 3 on this project for which I carried out the mock-up and development in Angular.",
+    },
+    {
+      href: "http://jess2410.github.io/Complete-Responsive-Website/",
+      name: "Brand",
+      img: "/assets/brand.jpg",
+      description:
+        "This website was created as part of my training in order to improve myself in the field of front-end.",
+    },
+    {
+      href: "#",
+      name: "Chapito",
+      img: "/assets/chapito.jpeg",
+      description:
+        "This logo was made for an application in progress by a team of developers. This is a fast delivery company.",
+    },
+    {
+      href: "http://github.com/Jess2410/amazona",
+      name: "Amazona",
+      img: "/assets/amazona.jpg",
+      description:
+        "This website was created as part of my training in order to improve myself in the field of front-end.",
+    },
   ],
 };
 export const skillsData = {
@@ -40,7 +96,7 @@ export const skillsData = {
     { name: "Next Js", img: "/assets/next.jpg" },
     { name: "Figma", img: "/assets/figma.png" },
     { name: "Procreate", img: "/assets/procreate.jpg" },
-    { name: "Design & Logos", img: "/assets/designLogo.png" },
+    { name: "Design & Logos", img: "/assets/designer.png" },
     { name: "UX/UI", img: "/assets/uxui.png" },
   ],
 };
